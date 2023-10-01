3f00e0c00a5a30aa269db2247473be2a2430c48b
Deenzz <139491326+Deenzz@users.noreply.github.com>
2023-10-01 14:48:18 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,68 +2,76 @@ document.addEventListener("DOMContentLoaded", function () {
     const calculateButton = document.getElementById("calculateButton");
     const weightInput = document.getElementById("weight");
     const resultDiv = document.getElementById("result");
-    const bromhexineDoseSpan = document.querySelector("#bromhexineDose span");
-    const azithromycinDoseSpan = document.querySelector("#azithromycinDose span");
-    const piritonDoseSpan = document.querySelector("#piritonDose span");
-    const augmentinDoseSpan = document.querySelector("#augmentinDose span");
-    const amoxicillinDoseSpan = document.querySelector("#amoxicillinDose span");
-    const cefuroximeDoseSpan = document.querySelector("#cefuroximeDose span");
-    const paracetamolDoseSpan = document.querySelector("#paracetamolDose span");
-    const prednisoloneDoseSpan = document.querySelector("#prednisoloneDose span");
-    const domperidoneDoseSpan = document.querySelector("#domperidoneDose span");
+    const medicationDosesList = document.getElementById("medicationDoses");
     const errorDiv = document.getElementById("error");
     const errorMessage = document.getElementById("errorMessage");
 
     calculateButton.addEventListener("click", function () {
+        // Clear previous results and errors
+        clearResults();
+
         const patientWeight = parseFloat(weightInput.value);
 
-        try {
-            const doses = calculateDoses(patientWeight);
-            displayDoses(doses);
-        } catch (error) {
-            displayError(error.message);
+        if (isNaN(patientWeight) || patientWeight <= 0) {
+            displayError("Please enter a valid positive weight.");
+        } else {
+            try {
+                const dosesAndNotes = calculateDoses(patientWeight);
+                displayDosesAndNotes(dosesAndNotes);
+            } catch (error) {
+                displayError(error.message);
+            }
         }
     });
 
     function calculateDoses(weight) {
-        if (weight <= 0 || isNaN(weight)) {
-            throw new Error("Weight should be a positive number.");
-        }
+        const medicationData = [
+            { name: "Paracetamol", dose: (weight * 5 * 15) / 250, note: "QID" },
+            { name: "Amoxicillin", dose: (weight * 5 * 15) / 250, note: "MAX DOSE: Amox 10ml TDS" },
+            { name: "Cephalexin", dose: (weight * 5 * 15) / 250, note: "MAX DOSE: Cephalexin 10ml QID" },
+            { name: "Cefaclor", dose: (weight * 5 * 15) / 250, note: "MAX DOSE: Cefaclor 5ml TDS" },
+            { name: "Cloxacilin", dose: (weight * 5 * 15) / 250, note: "MAX DOSE: Cloxacilin 10ml QID" },
+            { name: "Augmentin (228mg/5ml)", dose: (weight * 5 * 15) / 228, note: "BD/TDS for 5 days" },
+            { name: "Augmentin (312.5mg/5ml)", dose: (weight * 5 * 15) / 312.5, note: "BD/TDS for 5 days" },
+            { name: "Augmentin (457mg/5ml)", dose: (weight * 5 * 15) / 457, note: "BD/TDS for 5 days" },
+            { name: "Azithromycin", dose: (weight * 5 * 10) / 200, note: "BD for 3 days" },
+            { name: "Cefuroxime", dose: (weight * 5 * 10) / 125, note: "BD for 5 days" },
+            { name: "Erythromycin", dose: (weight * 5 * 20) / 200, note: "BD for 5 days" },
+            { name: "Cetirizine", dose: (weight * 5 * 0.25) / 5, note: "Use > 2 y/o , OD" },
+            { name: "Desloratadine", dose: (weight * 5 * 0.1) / 2.5, note: "Use > 1 y/o , OD" },
+            { name: "Piriton", dose: (weight * 5 * 0.1) / 4, note: "Use > 2 y/o , Max dose 5ml TDS" },
+            { name: "Bromhexine", dose: (weight * 5 * 0.3) / 4, note: "Use > 2 y/o , TDS" },
+            { name: "Benadryl", dose: (weight * 5 * 1) / 14, note: "Use > 2 y/o , Not for ASTHMA pt. Max dose 10ml TDS" },
+            { name: "Ibuprofen", dose: (weight * 5 * 5) / 100, note: "TDS/QID, Caution: GI bleed" },
+            { name: "Prednisolone", dose: (weight * 5 * 0.15) / 2.5, note: "TDS. AEBA can use 1mg/kg OD." },
+            { name: "Promethazine", dose: (weight * 5 * 0.2) / 5, note: "Use > 2 y/o , TDS (Antiemetic/Antihistamine)" },
+            { name: "Domperidone", dose: (weight * 5 * 0.25) / 5, note: "TDS" },
 
-        const doses = {
-            bromhexine: (weight * 0.3 * 5) / 4,      // Convert mg to ml
-            azithromycin: (weight * 10 * 5) / 200,   // Convert mg to ml
-            piriton: (weight * 0.1 * 5) / 4,         // Convert mg to ml
-            augmentin: (weight * 30 * 5) / 228,         // Convert mg to ml
-            amoxicillin: (weight * 30 * 5) / 250,         // Convert mg to ml
-            cefuroxime: (weight * 10 * 5) / 125,         // Convert mg to ml
-            azithromycin: (weight * 12 * 5) / 200,         // Convert mg to ml
-            paracetamol: (weight * 15 * 5) / 250,         // Convert mg to ml
-            prednisolone: (weight * 1 * 5) / 3,         // Convert mg to ml
-            domperidone: (weight * 0.25 * 5) / 5,         // Convert mg to ml
-        };
+        ];
 
-        return doses;
+        return medicationData;
     }
 
-    function displayDoses(doses) {
-        bromhexineDoseSpan.textContent = doses.bromhexine.toFixed(2);
-        azithromycinDoseSpan.textContent = doses.azithromycin.toFixed(2);
-        piritonDoseSpan.textContent = doses.piriton.toFixed(2);
-        augmentinDoseSpan.textContent = doses.augmentin.toFixed(2);
-        amoxicillinDoseSpan.textContent = doses.amoxicillin.toFixed(2);
-        cefuroximeDoseSpan.textContent = doses.cefuroxime.toFixed(2);
-        azithromycinDoseSpan.textContent = doses.azithromycin.toFixed(2);
-        paracetamolDoseSpan.textContent = doses.paracetamol.toFixed(2);
-        prednisoloneDoseSpan.textContent = doses.prednisolone.toFixed(2);
-        domperidoneDoseSpan.textContent = doses.domperidone.toFixed(2);
+    function displayDosesAndNotes(dosesAndNotes) {
+        dosesAndNotes.forEach((medication) => {
+            const listItem = document.createElement("li");
+            listItem.textContent = `${medication.name}: ${medication.dose.toFixed(2)} ml - Note: ${medication.note}`;
+            medicationDosesList.appendChild(listItem);
+        });
+
         resultDiv.classList.remove("hidden");
-        errorDiv.classList.add("hidden");
     }
 
     function displayError(message) {
         errorMessage.textContent = message;
-        resultDiv.classList.add("hidden");
         errorDiv.classList.remove("hidden");
+    }
+
+    function clearResults() {
+        while (medicationDosesList.firstChild) {
+            medicationDosesList.removeChild(medicationDosesList.firstChild);
+        }
+        resultDiv.classList.add("hidden");
+        errorDiv.classList.add("hidden");
     }
 });
